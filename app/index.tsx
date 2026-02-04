e9ce8d734cd5f08d824bf17d4c241f2e61c7a5cc
NxGin97 <206642376+NxGin97@users.noreply.github.com>
2026-02-04 12:22:35 -0700

    setting up page layout for facebook marketplace home page. -Nat, Alex, Rishabh

--- app/index.tsx
+++ app/index.tsx
@@ -9,7 +9,78 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+
+      {/*Header*/}
+      <View>
+        <Text>hamburgerIcon</Text>
+        <Text>Marketplace</Text>
+        <Text>messengerIcon</Text>
+        <Text>profileIcon</Text>
+        <Text>Search icon</Text>
+      </View>
+      <View>
+        <Text>Sell</Text>
+        <Text>For you</Text>
+        <Text>Local</Text>
+        <Text>Categories</Text>
+      </View>
+      <View>
+        <Text>Today's picks</Text>
+        <Text>LocationIcon</Text>
+        <Text>Calgary, AB</Text>
+      </View>
+    
+    {/*Might have to be flex*/}
+      <View> 
+        <Text>Today's picks</Text>
+        <Text>LocationIcon</Text>
+        <Text>Calgary, AB</Text>
+      </View>
+
+    {/*Might have to be grid*/}
+      <View> 
+        <View>
+          <Text>Img</Text>
+          <Text>Just Listed use absolute positioning</Text>
+          <Text>Free • Bar Stools</Text>
+        </View>
+        <View>
+          <Text>Img</Text>
+          <Text>Just Listed use absolute positioning</Text>
+          <Text>$1 • Closet Cleanout</Text>
+        </View>
+        <View>
+          <Text>Img</Text>
+          <Text>Just Listed use absolute positioning</Text>
+          <Text>$10 • Mens socks $10 pe</Text>
+          <Text>...</Text>
+        </View>
+        <View>
+          <Text>Img</Text>
+          <Text>Just Listed use absolute positioning</Text>
+          <Text>$10 • Tropical Plants in W </Text>
+          <Text>...</Text>
+        </View>
+        <View>
+          <Text>Img</Text>
+          <Text>$25 <Text>$30 strikethrough</Text> • Oversized plush bla</Text>
+          <Text>...</Text>
+        </View>
+        <View>
+          <Text>Img</Text>
+          <Text>$10 • woman knit</Text>
+        </View>
+      </View>
+
+    {/*Nav Bar*/}
+      <View>
+        <Text>HomeIcon</Text>
+        <Text>ReelsIcon</Text>
+        <Text>SelectedMarketIcon</Text>
+        <Text>GroupIcon</Text>
+        <Text>NotificationIcon</Text>
+        <Text>ProfileIcon</Text>
+      </View>
     </View>
   );
 }
